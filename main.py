f08cba2bad75db73d14551092ba747f0f6fd556e
Jungwoo Kim <kimjw0623@kaist.ac.kr>
2022-08-03 22:21:24 +0900

    comtain db

--- main.py
+++ main.py
@@ -110,53 +110,57 @@ if GET_NEW_ID:
 sel = select(summoner_id_table.c.id) # get all column if blank 
 get_id_result = connId.execute(sel)
 
-line = 0
-for row in get_id_result:
-    print(row.id)
-    line += 1
-print(line)
+# line = 0
+# for row in get_id_result:
+#     print(row.id)
+#     line += 1
+# print(line)
 ######################################################
 
 # Create DB
-# masteryEngine = create_engine('sqlite:///summoner_mastery.db', echo=False)
+masteryEngine = create_engine('sqlite:///summoner_mastery.db', echo=False)
 
-# connMastery = masteryEngine.connect()
+connMastery = masteryEngine.connect()
 
-# connMastery.execute(text("drop table if exists summoner_mastery_table;")) # Is this work?
-# result = connMastery.execute(text("create table summoner_mastery_table (id NVARCHAR(63), sum INTEGER, aatrox INTEGER, ahri INTEGER, akali INTEGER, akshan INTEGER, alistar INTEGER, amumu INTEGER, anivia INTEGER, annie INTEGER, aphelios INTEGER, ashe INTEGER, aurelionsol INTEGER, azir INTEGER, bard INTEGER, belveth INTEGER, blitzcrank INTEGER, brand INTEGER, braum INTEGER, caitlyn INTEGER, camille INTEGER, cassiopeia INTEGER, chogath INTEGER, corki INTEGER, darius INTEGER, diana INTEGER, draven INTEGER, drmundo INTEGER, ekko INTEGER, elise INTEGER, evelynn INTEGER, ezreal INTEGER, gwen INTEGER, fiddlesticks INTEGER, fiora INTEGER, fizz INTEGER, galio INTEGER, gangplank INTEGER, garen INTEGER, gnar INTEGER, gragas INTEGER, graves INTEGER, hecarim INTEGER, heimerdinger INTEGER, illaoi INTEGER, irelia INTEGER, ivern INTEGER, janna INTEGER, jarvaniv INTEGER, jax INTEGER, jayce INTEGER, jhin INTEGER, jinx INTEGER, kaisa INTEGER, kalista INTEGER, karma INTEGER, karthus INTEGER, kassadin INTEGER, katarina INTEGER, kayle INTEGER, kayn INTEGER, kennen INTEGER, khazix INTEGER, kindred INTEGER, kled INTEGER, kogmaw INTEGER, leblanc INTEGER, leesin INTEGER, leona INTEGER, lillia INTEGER, lissandra INTEGER, lucian INTEGER, lulu INTEGER, lux INTEGER, malphite INTEGER, malzahar INTEGER, maokai INTEGER, masteryi INTEGER, missfortune INTEGER, monkeyking INTEGER, mordekaiser INTEGER, morgana INTEGER, nami INTEGER, nasus INTEGER, nautilus INTEGER, neeko INTEGER, nidalee INTEGER, nilah INTEGER, nocturne INTEGER, nunu INTEGER, olaf INTEGER, orianna INTEGER, ornn INTEGER, pantheon INTEGER, poppy INTEGER, pyke INTEGER, qiyana INTEGER, quinn INTEGER, rakan INTEGER, rammus INTEGER, reksai INTEGER, rell INTEGER, renata INTEGER, renekton INTEGER, rengar INTEGER, riven INTEGER, rumble INTEGER, ryze INTEGER, samira INTEGER, sejuani INTEGER, senna INTEGER, seraphine INTEGER, sett INTEGER, shaco INTEGER, shen INTEGER, shyvana INTEGER, singed INTEGER, sion INTEGER, sivir INTEGER, skarner INTEGER, sona INTEGER, soraka INTEGER, swain INTEGER, sylas INTEGER, syndra INTEGER, tahmkench INTEGER, taliyah INTEGER, talon INTEGER, taric INTEGER, teemo INTEGER, thresh INTEGER, tristana INTEGER, trundle INTEGER, tryndamere INTEGER, twistedfate INTEGER, twitch INTEGER, udyr INTEGER, urgot INTEGER, varus INTEGER, vayne INTEGER, veigar INTEGER, velkoz INTEGER, vex INTEGER, vi INTEGER, viego INTEGER, viktor INTEGER, vladimir INTEGER, volibear INTEGER, warwick INTEGER, xayah INTEGER, xerath INTEGER, xinzhao INTEGER, yasuo INTEGER, yone INTEGER, yorick INTEGER, yuumi INTEGER, zac INTEGER, zed INTEGER, zeri INTEGER, ziggs INTEGER, zilean INTEGER, zoe INTEGER, zyra INTEGER, primary key (id));"))
+connMastery.execute(text("drop table if exists summoner_mastery_table;")) # Is this work?
+result = connMastery.execute(text("create table summoner_mastery_table (id NVARCHAR(63), sum INTEGER, aatrox INTEGER, ahri INTEGER, akali INTEGER, akshan INTEGER, alistar INTEGER, amumu INTEGER, anivia INTEGER, annie INTEGER, aphelios INTEGER, ashe INTEGER, aurelionsol INTEGER, azir INTEGER, bard INTEGER, belveth INTEGER, blitzcrank INTEGER, brand INTEGER, braum INTEGER, caitlyn INTEGER, camille INTEGER, cassiopeia INTEGER, chogath INTEGER, corki INTEGER, darius INTEGER, diana INTEGER, draven INTEGER, drmundo INTEGER, ekko INTEGER, elise INTEGER, evelynn INTEGER, ezreal INTEGER, gwen INTEGER, fiddlesticks INTEGER, fiora INTEGER, fizz INTEGER, galio INTEGER, gangplank INTEGER, garen INTEGER, gnar INTEGER, gragas INTEGER, graves INTEGER, hecarim INTEGER, heimerdinger INTEGER, illaoi INTEGER, irelia INTEGER, ivern INTEGER, janna INTEGER, jarvaniv INTEGER, jax INTEGER, jayce INTEGER, jhin INTEGER, jinx INTEGER, kaisa INTEGER, kalista INTEGER, karma INTEGER, karthus INTEGER, kassadin INTEGER, katarina INTEGER, kayle INTEGER, kayn INTEGER, kennen INTEGER, khazix INTEGER, kindred INTEGER, kled INTEGER, kogmaw INTEGER, leblanc INTEGER, leesin INTEGER, leona INTEGER, lillia INTEGER, lissandra INTEGER, lucian INTEGER, lulu INTEGER, lux INTEGER, malphite INTEGER, malzahar INTEGER, maokai INTEGER, masteryi INTEGER, missfortune INTEGER, monkeyking INTEGER, mordekaiser INTEGER, morgana INTEGER, nami INTEGER, nasus INTEGER, nautilus INTEGER, neeko INTEGER, nidalee INTEGER, nilah INTEGER, nocturne INTEGER, nunu INTEGER, olaf INTEGER, orianna INTEGER, ornn INTEGER, pantheon INTEGER, poppy INTEGER, pyke INTEGER, qiyana INTEGER, quinn INTEGER, rakan INTEGER, rammus INTEGER, reksai INTEGER, rell INTEGER, renata INTEGER, renekton INTEGER, rengar INTEGER, riven INTEGER, rumble INTEGER, ryze INTEGER, samira INTEGER, sejuani INTEGER, senna INTEGER, seraphine INTEGER, sett INTEGER, shaco INTEGER, shen INTEGER, shyvana INTEGER, singed INTEGER, sion INTEGER, sivir INTEGER, skarner INTEGER, sona INTEGER, soraka INTEGER, swain INTEGER, sylas INTEGER, syndra INTEGER, tahmkench INTEGER, taliyah INTEGER, talon INTEGER, taric INTEGER, teemo INTEGER, thresh INTEGER, tristana INTEGER, trundle INTEGER, tryndamere INTEGER, twistedfate INTEGER, twitch INTEGER, udyr INTEGER, urgot INTEGER, varus INTEGER, vayne INTEGER, veigar INTEGER, velkoz INTEGER, vex INTEGER, vi INTEGER, viego INTEGER, viktor INTEGER, vladimir INTEGER, volibear INTEGER, warwick INTEGER, xayah INTEGER, xerath INTEGER, xinzhao INTEGER, yasuo INTEGER, yone INTEGER, yorick INTEGER, yuumi INTEGER, zac INTEGER, zed INTEGER, zeri INTEGER, ziggs INTEGER, zilean INTEGER, zoe INTEGER, zyra INTEGER, primary key (id));"))
 
-# meta.reflect(bind = masteryEngine)
+meta.reflect(bind = masteryEngine)
 
-# # Get mastery points
-# URL_MASTERY_BY_ID = 'https://{region}.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/{summonerId}?api_key={apiKey}'
+# Get mastery points
+URL_MASTERY_BY_ID = 'https://{region}.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/{summonerId}?api_key={apiKey}'
 
-# idList = connId.execute(sel)
-# apiCall = 0
-# for row in idList:
-#     summonerMasteryResponse = requests.get(URL_MASTERY_BY_ID.format(
-#                                         region = REGION,
-#                                         summonerId = row.id,
-#                                         apiKey = apikey))
-#     apiCall += 1
-#     summonerMasteryInfo = json.loads(summonerMasteryResponse.text)
-#     summonerMasteryDic = {}
-#     summonerMasteryDic['id'] = row.id
-#     pointSum = 0
-#     for elem in summonerMasteryInfo:
-#         champId = elem['championId']
-#         champPoints = elem['championPoints']
-#         pointSum += champPoints
-#         summonerMasteryDic[ID_TO_CHAMP[champId]] = champPoints
+idList = connId.execute(sel)
+apiCall = 0
+for row in idList:
+    summonerMasteryResponse = requests.get(URL_MASTERY_BY_ID.format(
+                                        region = REGION,
+                                        summonerId = row.id,
+                                        apiKey = apikey))
+    apiCall += 1
+    summonerMasteryInfo = json.loads(summonerMasteryResponse.text)
+    summonerMasteryDic = {}
+    summonerMasteryDic['id'] = row.id
+    pointSum = 0
+    for elem in summonerMasteryInfo:
+        champId = elem['championId']
+        champPoints = elem['championPoints']
+        pointSum += champPoints
+        summonerMasteryDic[ID_TO_CHAMP[champId]] = champPoints
         
-#     summonerMasteryDic['sum'] = pointSum
-#     summonerMasteryDf = pd.DataFrame(summonerMasteryDic, index = [0])
-#     summonerMasteryDf.to_sql('summoner_mastery_table', connMastery, if_exists='append', index=False)
-#     if apiCall%90 == 0:
-#            print('sleep')
-#            time.sleep(130)
+    summonerMasteryDic['sum'] = pointSum
+    print(pointSum)
+    summonerMasteryDf = pd.DataFrame(summonerMasteryDic, index = [0])
+    summonerMasteryDf.to_sql('summoner_mastery_table', connMastery, if_exists='append', index=False)
+    if apiCall%95 == 0:
+        print('sleep')
+        time.sleep(130)
 
-# sel = select(meta.tables['summoner_mastery_table']) # get all column if blank 
-# result = connMastery.execute(sel)
+sel = select(meta.tables['summoner_mastery_table']) # get all column if blank 
+result = connMastery.execute(sel)
 
-# for row in result:
-#     print(row)
+line = 0
+for row in result:
+    print(row)
+    line += 1
+print(line)
